6054861355e2783f5bf81ea05686a1f957bc0237
SuryaS1729 <surya.curiousfusion@gmail.com>
2024-12-22 17:10:14 +0530

    frontend added with a quote component

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,16 @@
+
+export const Quote = ()=>{
+    return <div className="bg-slate-200 h-screen flex justify-center flex-col ">
+
+        <div className="flex justify-center px-4 ">
+            <div className="max-w-lg"> 
+                 <div className=" text-3xl font-bold">"The customer support I recieved was exceptional. The    support team went above and beyond to address my concerns"
+                 </div> 
+                 <div className="text-xl font-semibold max-w-md mt-4"> Julius Winfield</div>
+                <div className="text-sm font-light max-w-md text-slate-400"> CEO | Acme Corp</div>
+             </div>
+              
+        </div>
+      
+    </div>
+}
